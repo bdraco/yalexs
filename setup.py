f444315e10fb464e7546378ee79f74a828f7aaa6
J. Nick Koston <nick@koston.org>
2021-12-23 09:45:17 -1000

    Bump version: 1.1.15 → 1.1.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="yalexs",
-    version="1.1.15",
+    version="1.1.16",
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
