072901ed8225504bc794358e0dd64b0b3d98353a
J. Nick Koston <nick@koston.org>
2023-05-17 09:08:57 -0500

    Bump version: 1.3.3 → 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="yalexs",
-    version="1.3.3",
+    version="1.4.0",
     python_requires=">=3.9",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
