5466b3c33678b837ac046efa70cf43dea10d0eb9
J. Nick Koston <nick@koston.org>
2023-05-17 15:48:02 -0500

    Update tokens for Yale Home (#66)

--- yalexs/const.py
+++ yalexs/const.py
@@ -28,3 +28,18 @@ BRANDING = {
     Brand.YALE_ACCESS: "yale",
     Brand.YALE_HOME: "yale",
 }
+
+PUBNUB_TOKENS = {
+    Brand.AUGUST: {
+        "subscribe": "sub-c-1030e062-0ebe-11e5-a5c2-0619f8945a4f",
+        "publish": "pub-c-567d7f2d-270a-438a-a785-f0af12ad8312",
+    },
+    Brand.YALE_ACCESS: {
+        "subscribe": "sub-c-1030e062-0ebe-11e5-a5c2-0619f8945a4f",
+        "publish": "pub-c-567d7f2d-270a-438a-a785-f0af12ad8312f",
+    },
+    Brand.YALE_HOME: {
+        "subscribe": "sub-c-c9c38d4d-5796-46c9-9262-af20cf6a1d42",
+        "publish": "pub-c-353e8881-cf58-4b26-9baf-96f296de0677",
+    },
+}
