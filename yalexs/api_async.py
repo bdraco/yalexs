d066cb23cc876167f662b2c2bcf3621c42d76b80
J. Nick Koston <nick@koston.org>
2024-02-08 13:47:29 -0600

    chore: lint fixes

--- yalexs/api_async.py
+++ yalexs/api_async.py
@@ -1,4 +1,5 @@
 """Api calls for sync."""
+
 from __future__ import annotations
 
 import asyncio
--- yalexs/backports/functools.py
+++ yalexs/backports/functools.py
@@ -1,4 +1,5 @@
 """Functools backports from standard lib."""
+
 from __future__ import annotations
 
 from collections.abc import Callable
@@ -31,12 +32,10 @@ class cached_property(Generic[_T]):  # pylint: disable=invalid-name
             )
 
     @overload
-    def __get__(self, instance: None, owner: type[Any] | None = None) -> Any:
-        ...
+    def __get__(self, instance: None, owner: type[Any] | None = None) -> Any: ...
 
     @overload
-    def __get__(self, instance: Any, owner: type[Any] | None = None) -> _T:
-        ...
+    def __get__(self, instance: Any, owner: type[Any] | None = None) -> _T: ...
 
     def __get__(self, instance: Any | None, owner: type[Any] | None = None) -> _T | Any:
         """Get."""
