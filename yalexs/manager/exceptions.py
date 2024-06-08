8293f91831e3a446596ad1357eebfce48325f13c
J. Nick Koston <nick@koston.org>
2024-06-08 18:12:34 -0500

    feat: add manager

---
+++ yalexs/manager/exceptions.py
@@ -0,0 +1,14 @@
+"""Exceptions for errors."""
+from __future__ import annotations
+
+
+class RequireValidation(Exception):
+    """Error to indicate we require validation (2fa)."""
+
+
+class CannotConnect(Exception):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(Exception):
+    """Error to indicate there is invalid auth."""
