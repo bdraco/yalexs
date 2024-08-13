a41474d98bc1f56fe014f9a1ac09c32aed7afc14
J. Nick Koston <nick@koston.org>
2024-08-13 07:54:21 -0500

    fix: cleanup _async_dict_to_api for oauth conversion (#138)

--- yalexs/api_async.py
+++ yalexs/api_async.py
@@ -379,14 +379,9 @@ class ApiAsync(ApiCommon):
         ).headers[HEADER_AUGUST_ACCESS_TOKEN]
 
     async def _async_dict_to_api(self, api_dict: dict[str, Any]) -> ClientResponse:
-        url = api_dict["url"]
-        method = api_dict["method"]
-        access_token = api_dict.get("access_token", None)
-        del api_dict["url"]
-        del api_dict["method"]
-        if access_token:
-            del api_dict["access_token"]
-
+        url = api_dict.pop("url")
+        method = api_dict.pop("method")
+        access_token = api_dict.pop("access_token", None)
         payload = api_dict.get("params") or api_dict.get("json")
 
         if "headers" not in api_dict:
