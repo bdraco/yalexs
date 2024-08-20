7c9e37668ec87516c0310eed01e50ccaa5fca252
J. Nick Koston <nick@koston.org>
2024-08-20 10:42:49 -0500

    fix: ensure subclassed AugustApiAIOHTTPError is back-compat

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -34,3 +34,18 @@ def test_august_api_aio_http_error_reraise():
     assert ex.auth_failed is True
     assert ex.aiohttp_client_error is mock_client_response_error
     assert ex.args == ("test",)
+
+
+def test_subclassed_august_api_aio_http_error_reraise():
+    mock_client_response_error = ClientResponseError(
+        mock.MagicMock(),
+        mock.MagicMock(),
+        status=401,
+    )
+    try:
+        raise YaleApiError("test", mock_client_response_error)
+    except AugustApiAIOHTTPError as ex:
+        assert str(ex) == "test"
+        assert ex.auth_failed is True
+        assert ex.aiohttp_client_error is mock_client_response_error
+        assert ex.args == ("test",)
