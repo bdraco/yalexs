f715ae1d8abb878f5707017b6da66f4f91423f89
J. Nick Koston <nick@koston.org>
2024-09-03 07:15:35 -1000

    fix: remove yale global pubnub tokens (#180)

--- yalexs/const.py
+++ yalexs/const.py
@@ -1,5 +1,6 @@
 """Constants."""
 
+from __future__ import annotations
 from .backports.enum import StrEnum
 from dataclasses import dataclass
 
@@ -28,6 +29,8 @@ class BrandConfig:
     require_oauth: bool
     base_url: str
     configuration_url: str
+    pubnub_subscribe_token: str | None
+    pubnub_publish_token: str | None
 
 
 HEADER_VALUE_API_KEY_OLD = "7cab4bbd-2693-4fc1-b99b-dec0fb20f9d4"
@@ -53,6 +56,8 @@ BRAND_CONFIG: dict[Brand, BrandConfig] = {
         require_oauth=False,
         base_url="https://api-production.august.com",
         configuration_url="https://account.august.com",
+        pubnub_subscribe_token="sub-c-1030e062-0ebe-11e5-a5c2-0619f8945a4f",  # nosec
+        pubnub_publish_token="pub-c-567d7f2d-270a-438a-a785-f0af12ad8312",  # nosec
     ),
     Brand.YALE_ACCESS: BrandConfig(
         name="Yale Access",
@@ -65,6 +70,8 @@ BRAND_CONFIG: dict[Brand, BrandConfig] = {
         require_oauth=False,
         base_url="https://api-production.august.com",
         configuration_url="https://account.august.com",
+        pubnub_subscribe_token="sub-c-1030e062-0ebe-11e5-a5c2-0619f8945a4f",  # nosec
+        pubnub_publish_token="pub-c-567d7f2d-270a-438a-a785-f0af12ad8312",  # nosec
     ),
     Brand.YALE_HOME: BrandConfig(
         name="Yale Home",
@@ -77,6 +84,8 @@ BRAND_CONFIG: dict[Brand, BrandConfig] = {
         require_oauth=False,
         base_url="https://api.aaecosystem.com",
         configuration_url="https://account.aaecosystem.com",
+        pubnub_subscribe_token="sub-c-c9c38d4d-5796-46c9-9262-af20cf6a1d42",  # nosec
+        pubnub_publish_token="pub-c-353e8881-cf58-4b26-9baf-96f296de0677",  # nosec
     ),
     Brand.YALE_GLOBAL: BrandConfig(
         name="Yale Global",
@@ -92,6 +101,12 @@ BRAND_CONFIG: dict[Brand, BrandConfig] = {
         require_oauth=True,
         base_url="https://api.aaecosystem.com",
         configuration_url="https://account.aaecosystem.com",
+        # This brand uses WebSockets and has migrated
+        # away from PubNub for this purpose which is great
+        # because its one less credential we have to expose
+        # to the user
+        pubnub_publish_token=None,
+        pubnub_subscribe_token=None,
     ),
 }
 
@@ -110,26 +125,4 @@ BASE_URLS = {
 CONFIGURATION_URLS = {
     brand: brand_config.configuration_url
     for brand, brand_config in BRAND_CONFIG.items()
-}
-
-PUBNUB_TOKENS = {
-    Brand.AUGUST: {
-        "subscribe": "sub-c-1030e062-0ebe-11e5-a5c2-0619f8945a4f",
-        "publish": "pub-c-567d7f2d-270a-438a-a785-f0af12ad8312",
-    },
-    Brand.YALE_ACCESS: {
-        "subscribe": "sub-c-1030e062-0ebe-11e5-a5c2-0619f8945a4f",
-        "publish": "pub-c-567d7f2d-270a-438a-a785-f0af12ad8312f",
-    },
-    Brand.YALE_HOME: {
-        "subscribe": "sub-c-c9c38d4d-5796-46c9-9262-af20cf6a1d42",
-        "publish": "pub-c-353e8881-cf58-4b26-9baf-96f296de0677",
-    },
-    # YALE_GLOBAL will eventually WebSockets for push updates
-    # but for now we will use the same as YALE_HOME until we
-    # can finish the WebSockets implementation
-    Brand.YALE_GLOBAL: {
-        "subscribe": "sub-c-c9c38d4d-5796-46c9-9262-af20cf6a1d42",
-        "publish": "pub-c-353e8881-cf58-4b26-9baf-96f296de0677",
-    },
 }
